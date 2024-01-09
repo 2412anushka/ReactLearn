171f23b28a6f8ca35aacad11407912852fba4f78
Anushka <anushkasy2003@gmail.com>
2024-01-09 10:13:09 +0530

    files added

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id}=useParams()
+    const {data:blog,error,isPending} = useFetch("https://my-json-server.typicode.com/2412anushka/ReactLearn/blogs/"+id)
     return ( 
         <div className="blog-details">
-            <h2>Blog Details - {id}</h2>
+            {error && <p>{error}</p>}
+            {isPending && <div>Loading...</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <h4>- {blog.author}</h4>
+                    <div>{blog.des}</div>
+                </article>
+            )}
         </div>
      );
 }
--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,7 +1,49 @@
+import {useState} from "react";
+
 const Create = () => {
+    const [title,setTitle]=useState('')
+    const [body,setBody]=useState('')
+    const [author,setAuthor]=useState('Mario')
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const blog={title,body,author}
+        fetch("https://my-json-server.typicode.com/2412anushka/ReactLearn/blogs", {
+            method:'POST',
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(blog)
+        })
+        .then(()=>{
+            console.log("new blog added");
+        })
+    }
     return ( 
         <div className="create">
-            <h2>Add New Blogs</h2>
+            <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog Title:</label>
+                <input 
+                type="text" 
+                required
+                value={title}
+                onChange={(e)=> setTitle(e.target.value)}
+                />
+                <label>Blog Description:</label>
+                <textarea 
+                required
+                value={body}
+                onChange={(e)=> setBody(e.target.value)}>
+                </textarea>
+                <label>Blog Author:</label>
+                <select
+                    value={author}
+                    onChange={(e)=> setAuthor(e.target.value)}
+                >
+                    <option value="Mario">Mario</option>
+                    <option value="Tom">Tom</option>
+                    <option value="Michelle">Michelle</option>
+                </select>
+                <button>Add a blog</button>
+            </form>
         </div>
      );
 }
